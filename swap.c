1cad66009eafd69b6986eee2987b5a572e1fbbd2
Felix Meissner <felixmeissner@me.com>
2017-04-13 18:55:12 +0200

    only main function

--- swap.c
+++
@@ -1,7 +0,0 @@
-#include "swap.h"
-
-void swap(int& x, int& y){
-  int temp = y;
-  y = x;
-  x = temp;
-}
--- swap.h
+++
@@ -1,7 +0,0 @@
-// Tausche zwei Element gegeneinander aus.
-#ifndef swapH
-#define swapH
-
-void swap(int& x, int& y);
-
-#endif
